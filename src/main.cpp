40cb8169f2536cff9f8adf30aba5abf55e9d6c10
Andre Escorel <gustavo.escorel@gmail.com>
2023-11-09 15:38:37 -0300

    RGB LED implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,56 @@
 
-#include <WiFi.h>
-#include <DNSServer.h>
-#include <WebServer.h>
-#include <WiFiManager.h>
+#include <Arduino.h>
+// #include "Freenove_WS2812_Lib_for_ESP32.h"
+/*
+- implementar led de status + buzzer
+   - led verde + buzzer se RTC ok; duração 1s
+   - led vermelho + buzzer se falhar RTC; duração 1s
+   - apagar led e buzzer
+   - passar pra teste de imput humano
+- teste de input humano
+   - regra dos 3 segundos (esperar intervenção humana)
+   - entradas 1 a 4 (3 segundos cada)
+      - passar p proximo sempre que imput = ok
+      - esperar 3 segundos pra falhar
+   - upload to server
+      - led azul se ok; duração 1s
+      - led vermelho se erroç duração 1s
+- adicionar quinta saida (flush)
+  - teste de multiplas entradas
+*/
+#include "Freenove_WS2812_Lib_for_ESP32.h" //biblioteca do led RGB do DEVKIT Solo
 
-WebServer webServer(80);
+#define LEDS_COUNT 8
+#define LEDS_PIN 18
+#define CHANNEL 0
 
-const byte DNS_PORT = 53;
-IPAddress apIP(192, 168, 1, 1);
-DNSServer dnsServer;
+#define RGB_RED 255, 0, 0
+#define RGB_BLUE 0, 0, 255
+#define RGB_GREEN 0, 255, 0
+#define RGB_WHITE 255, 255, 255
+
+Freenove_ESP32_WS2812 RGB = Freenove_ESP32_WS2812(LEDS_COUNT, LEDS_PIN, CHANNEL, TYPE_GRB);
+
+#define SETUP_RGB() \
+   RGB.begin();     \
+   RGB.setBrightness(255)
+
+#define RGB_COLOR(COLOR)          \
+   RGB.setLedColorData(0, COLOR); \
+   RGB.show()
+
+#define RGB_OFF() RGB_COLOR((0, 0, 0))
 
 void setup()
 {
-  WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-  WiFi.softAP("DNSServer example");
-
-  // modify TTL associated  with the domain name (in seconds)
-  // default is 60 seconds
-  dnsServer.setTTL(300);
-  // set which return code will be used for all other domains (e.g. sending
-  // ServerFailure instead of NonExistentDomain will reduce number of queries
-  // sent by clients)
-  // default is DNSReplyCode::NonExistentDomain
-  dnsServer.setErrorReplyCode(DNSReplyCode::ServerFailure);
-
-  // start DNS server for a specific domain name
-  dnsServer.start(DNS_PORT, "www.example.com", apIP);
-
-  // simple HTTP server to see that DNS server is working
-  webServer.onNotFound([]()
-                       {
-    String message = "Hello World!\n\n";
-    message += "URI: ";
-    message += webServer.uri();
-
-    webServer.send(200, "text/plain", message); });
-  webServer.begin();
+   Serial.begin(115200);
+   SETUP_RGB();
 }
 
 void loop()
 {
-  dnsServer.processNextRequest();
-  webServer.handleClient();
+   RGB_COLOR(RGB_WHITE);
+   delay(1000);
+   RGB_OFF();
+   delay(1000);
 }
